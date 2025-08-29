7f0c0b92830cf2bcdf20684aa15d7f58a0954720
joaxo30 <149344139+joaxo30@users.noreply.github.com>
2025-08-28 23:12:14 -0400

    Update models.py

--- proyecto/proyectoAPP/models.py
+++ proyecto/proyectoAPP/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 # Create your models here.
 class registro(models.Model):
-    nombre =models.CharField(max_length=30)
-    apellido =models.CharField(max_length=30)
-    edad = models.IntegerField()
-    fecha_nacimiento = models.DateField(null=True, blank=True)
+    nombre =models.CharField(max_length=100)
+    apellido =models.CharField(max_length=100)
+    edad = models.PositiveIntegerField()
+    fecha_nacimiento = models.DateField()
+    descripcion = models.TextField()
+    imagen = models.ImagenField(upload_to='imagenes/', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
